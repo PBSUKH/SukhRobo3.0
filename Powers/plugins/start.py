c758695268b8925eb56870c2438651e515b8c4cf
badmunda98 <152181482+badmunda98@users.noreply.github.com>
2023-12-01 10:18:53 +0530

    Update start.py

--- Powers/plugins/start.py
+++ Powers/plugins/start.py
@@ -110,7 +110,8 @@ async def start(c: Gojo, m: Message):
             🙋𝐇ᴇʏ 🥀
 ๏ I'M SQUARE YOUR GROUP
 THE MOST POWERFUL MANGMENT ➕ MUSIC & CHATBOT OR MANY MORE
-➻ ANY HELP VISIT @THE_DRAMA_CLUB_01 THANKS FOR USING 🥀➣Bᴀᴅ❤︎ ᴍᴜɴᴅᴀ ➻ >•😈 BOTS."""
+➻ ANY HELP VISIT @THE_DRAMA_CLUB_01 THANKS FOR USING 🥀➣Bᴀᴅ❤︎ ᴍᴜɴᴅᴀ ➻ >•😈 BOTS.
+"""
 
             await m.reply_photo(
                 photo=str(choice(StartPic)),
